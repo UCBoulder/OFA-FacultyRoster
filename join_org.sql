c26b5abd859fc96068f82fa28d08922319fc115f
Zach Cramer <121065404+za-cramer@users.noreply.github.com>
2025-07-23 11:06:51 -0600

    Create join_org.sql
    join org to roster (rename college for readability)

---
+++ join_org.sql
@@ -0,0 +1,21 @@
+create or replace table 
+TEST.BLD_OIT_DNA_IR.RPT_FACULTY_ROSTER_AND_CATALOG as 
+select roster.*,
+    CASE org.collegedesc
+        WHEN 'COLLEGE ARTS & SCIENCES' THEN 'College of Arts & Sciences'
+        WHEN 'DN,CE & AVC, SUMMR SESS' THEN 'Summer Session'
+        WHEN 'COLLEGE MEDIA,COMM&INFO' THEN 'CMDI (formerly CMCI)'
+        WHEN 'LEEDS SCHOOL OF BUSINESS' THEN 'Leeds School of Business'
+        WHEN 'SCHOOL OF EDUCATION'      THEN 'School of Education'
+        WHEN 'COLLEGE OF ENGR&APPLIED SCI' THEN 'College of Eng & Applied Sci'
+        WHEN 'COLLEGE OF ENGR&APPLIED SCI' THEN 'College of Eng & Applied Sci'
+        WHEN 'SCHOOL OF LAW'            THEN 'School of Law'
+        WHEN 'LIBRARIES'                THEN 'Libraries'
+        WHEN 'COLLEGE OF MUSIC'         THEN 'College of Music'
+        ELSE 'NA'
+    END AS collegedesc_new,
+    org.ASDIV
+from TEST.BLD_OIT_DNA_IR.RPT_FACULTY_ROSTER_AND_CATALOG_ORIGINAL roster
+left join CU_ORG org
+on roster.deptid = org.deptid
+;
